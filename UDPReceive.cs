2099a96f0a2add8db243d05d39cbbc8d7269d6b5
Grover Chen <groverchen1994@gmail.com>
2017-04-30 10:50:49 -0400

    first commit

---
+++ UDPReceive.cs
@@ -0,0 +1,159 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+using System.Collections.Generic; 
+using System.Runtime.Serialization.Formatters.Binary; 
+using System.IO;
+
+public class UDPReceive : MonoBehaviour {
+
+	// receiving Thread
+	Thread receiveThread;
+
+	// udpclient object
+	UdpClient client;
+
+	public int port;
+
+	public Vector3 swordPos;
+	public Vector3 swordRot;
+	public GameObject sword;
+
+	// infos
+	public string lastReceivedUDPPacket="";
+	public string allReceivedUDPPackets=""; // clean up this from time to time!
+
+	// start from unity3d
+	public void Start()
+	{
+		init();
+	}
+
+	// OnGUI
+	void OnGUI()
+	{
+		Rect rectObj=new Rect(40,10,200,400);
+		GUIStyle style = new GUIStyle();
+		style.alignment = TextAnchor.UpperLeft;
+		GUI.Box(rectObj,"# UDPReceive\n127.0.0.1 "+port+" #\n"
+			+ "shell> nc -u 127.0.0.1 : "+port+" \n"
+			+ "\nLast Packet: \n"+ lastReceivedUDPPacket
+			+ "\n\nAll Messages: \n"+allReceivedUDPPackets
+			,style);
+	}
+
+	// init
+	private void init()
+	{
+		print("UDPSend.init()");
+
+		// define port
+		port = 8051;
+
+		// status
+		print("Sending to 127.0.0.1 : "+port);
+		print("Test-Sending to this Port: nc -u 127.0.0.1  "+port+"");
+
+		receiveThread = new Thread(
+			new ThreadStart(ReceiveData));
+		receiveThread.IsBackground = true;
+		receiveThread.Start();
+	}
+
+	// receive thread
+	private  void ReceiveData()
+	{
+		client = new UdpClient(port);
+
+		while (true)
+		{
+			try
+			{
+				IPEndPoint anyIP = new IPEndPoint(IPAddress.Any, 0);
+				byte[] data = client.Receive(ref anyIP);
+
+				string text = Encoding.UTF8.GetString(data);
+
+				print(">> " + text);
+
+				StringToVector3(text);
+
+//				print ("sword.transform.position:" + swordPos);
+//				print ("sword.transform.rotation:" + swordRot);
+
+				lastReceivedUDPPacket=text;
+				allReceivedUDPPackets=allReceivedUDPPackets+text;
+			}
+			catch (Exception err)
+			{
+				print(err.ToString());
+			}
+		}
+	}
+
+	public void Update() {
+		sword.transform.position = swordPos;
+	}
+
+	// getLatestUDPPacket
+	// cleans up the rest
+	public string getLatestUDPPacket()
+	{
+		allReceivedUDPPackets="";
+		return lastReceivedUDPPacket;
+	}
+		
+	// Convert a byte array to an Object
+	public static Vector3 ByteArrayToObject(byte[] arrBytes)
+	{
+		using (var memStream = new MemoryStream())
+		{
+			var binForm = new BinaryFormatter();
+			memStream.Write(arrBytes, 0, arrBytes.Length);
+			memStream.Seek(0, SeekOrigin.Begin);
+			var obj = binForm.Deserialize(memStream);
+			return (Vector3)obj;
+		}
+	}
+
+	// (4.2, 3.0, 0.0)(0.0,0.0,0.0)
+	public void StringToVector3(string sVector)
+	{
+		string sVector_1 = "";
+		string sVector_2 = "";
+		// Remove the parentheses
+
+		if (sVector.StartsWith ("(") && sVector.EndsWith (")")) {
+			sVector = sVector.Substring (1, sVector.Length - 2);
+		}
+		// split the items
+		string[] sArray_tmp = sVector.Split(')');
+
+		string[] sArray_1 = sArray_tmp [0].Split (',');
+
+		string sArray_tmp_tmp = sArray_tmp [1].Substring (1, sArray_tmp [1].Length - 1);
+
+		string[] sArray_2 = sArray_tmp_tmp.Split(',');
+
+		// store as a Vector3
+		Vector3 result_1 = new Vector3(
+			float.Parse(sArray_1[0]),
+			float.Parse(sArray_1[1]),
+			float.Parse(sArray_1[2]));
+
+		swordPos = result_1;
+
+		Vector3 result_2 = new Vector3(
+			float.Parse(sArray_2[0]),
+			float.Parse(sArray_2[1]),
+			float.Parse(sArray_2[2]));
+
+		swordRot = result_2;
+	}
+}
---
+++ UDPSend.cs
@@ -0,0 +1,146 @@
+﻿/*
+ 
+    -----------------------
+    UDP-Send
+    -----------------------
+    // [url]http://msdn.microsoft.com/de-de/library/bb979228.aspx#ID0E3BAC[/url]
+   
+    // > gesendetes unter
+    // 127.0.0.1 : 8050 empfangen
+   
+    // nc -lu 127.0.0.1 8050
+ 
+        // todo: shutdown thread at the end
+*/
+using UnityEngine;
+using System.Collections;
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Collections.Generic; 
+using System.Runtime.Serialization.Formatters.Binary; 
+using System.IO;
+
+public class UDPSend : MonoBehaviour
+{
+	private static int localPort;
+
+	private string IP;  
+	public int port; 
+
+	public Vector3 swordPos = new Vector3(0,0,0);
+	public Vector3 swordRot = new Vector3(0,0,0);
+
+	public GameObject sword;
+
+	IPEndPoint remoteEndPoint;
+	UdpClient client;
+
+//	string strMessage="testing";
+
+	// start from unity3d
+	public void Start()
+	{
+		init();
+	}
+
+	// OnGUI
+	void OnGUI()
+	{
+		Rect rectObj=new Rect(40,10,200,400);
+		GUIStyle style = new GUIStyle();
+		style.alignment = TextAnchor.UpperLeft;
+		GUI.Box(rectObj,"# UDPSend-Data\n127.0.0.1 "+port+" #\n"
+			+ "shell> nc -lu 127.0.0.1  "+port+" \n"
+			,style);
+
+//		strMessage=GUI.TextField(new Rect(40,70,140,20),strMessage);
+//		if (GUI.Button(new Rect(40,100,40,20),"send"))
+//		{
+//			sendString(strMessage+"\n");
+//		}  
+//			
+	}
+
+	public void Update() {
+		swordPos = sword.transform.position;
+		swordRot = sword.transform.rotation.eulerAngles;
+		sendString (swordPos.ToString () + swordRot.ToString());
+		print ("sword position:" + swordPos.ToString());
+		print ("sword rotation:" + swordRot.ToString ());
+
+		if (Input.GetKey(KeyCode.W)) {
+			sword.transform.position = new Vector3(sword.transform.position.x + 0.2f, sword.transform.position.y, sword.transform.position.z);
+		}
+		if (Input.GetKey(KeyCode.S)) {
+			sword.transform.position = new Vector3 (sword.transform.position.x - 0.2f, sword.transform.position.y, sword.transform.position.z);
+		}
+		if (Input.GetKey(KeyCode.A)) {
+			sword.transform.position = new Vector3 (sword.transform.position.x, sword.transform.position.y + 0.2f, sword.transform.position.z);
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			sword.transform.position = new Vector3 (sword.transform.position.x, sword.transform.position.y - 0.2f, sword.transform.position.z);
+		}
+			
+	}
+
+
+	public void init()
+	{
+		print("UDPSend.init()");
+
+		IP="127.0.0.1";
+		port=8051;
+
+		remoteEndPoint = new IPEndPoint(IPAddress.Parse(IP), port);
+		client = new UdpClient();
+
+		// status
+		print("Sending to "+IP+" : "+port);
+		print("Testing: nc -lu "+IP+" : "+port);
+
+	}
+		
+
+	// send Data
+	private void sendString(string message)
+	{
+		try
+		{
+			byte[] data = Encoding.UTF8.GetBytes(message);
+
+			client.Send(data, data.Length, remoteEndPoint);
+			print ("data.lenth:" + data.Length);
+		}
+		catch (Exception err)
+		{
+			print(err.ToString());
+		}
+	}
+
+	// endless test
+	private void sendEndless(string testStr)
+	{
+		do
+		{
+			sendString(testStr);
+		}
+		while(true);
+	}
+
+	// Convert an object to a byte array
+	public static byte[] ObjectToByteArray(Vector3 obj)
+	{
+		BinaryFormatter bf = new BinaryFormatter();
+		using (var ms = new MemoryStream())
+		{
+			bf.Serialize(ms, obj);
+			return ms.ToArray();
+		}
+	}
+
+}
+
+
